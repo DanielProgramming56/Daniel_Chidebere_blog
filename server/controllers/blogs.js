f0e0dc702f6341cfd47552b60ab22cc258925998
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-24 21:47:24 +0100

    done

--- server/controllers/blogs.js
+++ server/controllers/blogs.js
@@ -9,6 +9,23 @@ export const getAllBlogs = async (req, res, next) => {
     }
 }
 
+
+export const getBlogById = async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const blogFound = await Blog.findById(id)
+        if (!blogFound)
+        {
+            const err = new Error("Blog Not Found")
+            next(err)
+            return;
+        }
+        res.status(200).json(blogFound)
+    } catch (error) {
+        next(error)
+    }
+}
+
 export const createBlog = async (req, res, next) => {
     try {
         const { title, content } = req.body
@@ -18,10 +35,39 @@ export const createBlog = async (req, res, next) => {
             const error = new Error("all filed are required")
             next(error)
         }
+        const userId = (req.user);
+        
+        if (!userId.isAdmin)
+        {
+            const err = new Error("Only Admin can post blog")
+            next(err)
+            return;
+        }
+        else{
         const createBlog = new Blog({title, content})
         await createBlog.save()
         res.json(createBlog)
+        }
     } catch (error) {
         next(error)
+    }
+}
+
+export const updateBlog = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const {title, content} = req.body
+
+        const updatedBlog = await Blog.findByIdAndUpdate(id, {title, content}, {new: true})
+
+        if (!updatedBlog)
+        {
+            const err  = new Error("couldn't find blog")
+            next(err)
+            return
+        }
+        res.status(201).json(updatedBlog)
+    } catch (error) {
+        
     }
 }
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { createBlog, getAllBlogs } from "../controllers/blogs.js"
+import { createBlog, getAllBlogs, getBlogById, updateBlog } from "../controllers/blogs.js"
 import { authGuard } from "../middleware/auth.js"
 const route = express.Router()
 
 route.get("/", getAllBlogs)
+route.get("/:id", getBlogById)
 route.post("/create-post", authGuard, createBlog)
+route.put("/update/:id", updateBlog)
 
 export default route
