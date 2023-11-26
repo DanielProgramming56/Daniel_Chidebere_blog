282cf6ff9c4d6ed3d9377d16eec109aa8b76fb97
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-26 01:21:07 +0100

    dic

--- Blog-client/src/components/Footer.jsx
+++ Blog-client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer >
       <div className="container-footer">
         <p>&copy; 2023 Daniel Chidebere Dev. All rights reserved.</p>
       </div>
