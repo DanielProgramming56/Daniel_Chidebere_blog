19766af88f5659f55473b3da371a401074683754
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-27 11:06:04 +0100

    done

--- Blog-client/src/components/ProtectRoutes.jsx
+++ Blog-client/src/components/ProtectRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 const ProtectRoutes = () => {
-  let admin = true;
+  let admin = false;
 
   return admin ? <Outlet /> : <Navigate to={"/"} />;
 };
